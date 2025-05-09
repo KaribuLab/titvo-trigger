f4798ab8ece6051e7d3654f14d05fc2997bd2d24
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-05-09 12:09:26 -0400

    feat: Añadir clases y lógica para generar URLs firmadas de archivos CLI, incluyendo la implementación del caso de uso y la actualización del repositorio para manejar la creación de archivos.

---
+++ src/app/cli-files/cli-files.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService, StorageService } from '@titvo/shared'
+import { CliFilesRepository } from '@trigger/core/cli-files/cli-files.repository'
+import { randomUUID } from 'crypto'
+import { GetCliFilesSignedUrlsInputDo, GetCliFilesSignedUrlsOutputDto } from '@trigger/app/cli-files/cli-files.dto'
+
+const ONE_DAY_IN_SECONDS = 60 * 60 * 24
+const ONE_DAY_IN_MS = ONE_DAY_IN_SECONDS * 1000
+
+@Injectable()
+export class GetCliFilesSignedUrlsUseCase {
+  constructor (private readonly configService: ConfigService, private readonly cliFilesRepository: CliFilesRepository, private readonly storageService: StorageService) {}
+  async process (input: GetCliFilesSignedUrlsInputDo): Promise<GetCliFilesSignedUrlsOutputDto> {
+    const bucketName = await this.configService.get('cli_files_bucket_name')
+    const presignedUrls = await Promise.all(input.files.map(async (file) => {
+      const fileKey = `temp/${input.batchId}/${file.name}`
+      const output = await this.storageService.getSignedUrl({
+        containerName: bucketName,
+        inputPath: fileKey,
+        filePath: fileKey,
+        contentType: file.contentType,
+        expiresIn: ONE_DAY_IN_SECONDS
+      })
+      await this.cliFilesRepository.create({
+        fileId: randomUUID(),
+        batchId: input.batchId,
+        fileKey,
+        tti: Date.now() + ONE_DAY_IN_MS
+      })
+      return {
+        [file.name]: output.url
+      }
+    }))
+    return {
+      message: 'Files urls generated successfully',
+      urls: presignedUrls.reduce((acc, curr) => ({ ...acc, ...curr }), {})
+    }
+  }
+}
--- src/core/cli-files/cli-files.repository.ts
+++ src/core/cli-files/cli-files.repository.ts
@@ -2,4 +2,5 @@ import { CliFileEntity } from '@trigger/core/cli-files/cli-files.entity'
 
 export abstract class CliFilesRepository {
   abstract findByBatchId (batchId: string): Promise<CliFileEntity[]>
+  abstract create (cliFile: CliFileEntity): Promise<void>
 }
